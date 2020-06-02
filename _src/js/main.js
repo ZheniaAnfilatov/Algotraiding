b6ef6768b02fd6e56293af3204970cefc1918cb1
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-06-02 20:23:08 +0300

    add burger menu

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-  
+  $('.menu__burger').click(function(event) {
+    $('.menu__burger,.menu__nav,.menu__burger--line').toggleClass('active');
+    $('body').toggleClass('lock')
+  });
 });
