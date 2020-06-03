65be852410eeaf9f24dc5ea97915920451f01ef9
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-06-03 19:39:01 +0300

    add section benefit

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
+
+  // меню бургер
   $('.menu__burger').click(function(event) {
     $('.menu__burger,.menu__nav,.menu__burger--line').toggleClass('active');
     $('body').toggleClass('lock')
   });
+
+  // слайдер
+  $('.benefit__slider').slick();
 });
