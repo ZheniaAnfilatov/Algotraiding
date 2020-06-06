86204d3aa584f84dfd0a2f20a7c4bf3f6bfce7b3
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-06-06 15:18:22 +0300

    add section reviews, PP

--- _src/js/main.js
+++ _src/js/main.js
@@ -17,11 +17,53 @@ $(document).ready(function () {
     dots: true,
     infinite: false,
   });
+
+  // слайдер reviews
+  $('.reviews__slider').slick({
+    arrows: false,
+    centerMode: true,
+    centerPadding: '290px',
+    slidesToShow: 1,
+    focusOnSelect: true,
+  });
   
   // видео
   var player;
   $('.video__play').on('click', function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
+      height: '100%',
+      width: '100%',
+      videoId: 'pT1EpTWPnyo',
+      events: {
+        'onReady': videoPlay,
+      }
+    });
+  })
+
+  $('.video__play1').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('reviews-playerOne', {
+      height: '100%',
+      width: '100%',
+      videoId: 'pT1EpTWPnyo',
+      events: {
+        'onReady': videoPlay,
+      }
+    });
+  })
+
+  $('.video__play1').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('reviews-playerTwo', {
+      height: '100%',
+      width: '100%',
+      videoId: 'pT1EpTWPnyo',
+      events: {
+        'onReady': videoPlay,
+      }
+    });
+  })
+
+  $('.video__play1').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('reviews-playerThree', {
       height: '100%',
       width: '100%',
       videoId: 'pT1EpTWPnyo',
